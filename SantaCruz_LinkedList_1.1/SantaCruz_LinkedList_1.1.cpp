74a39bad7773e1cf0d07fe584d798de343ef277a
Karl Nueckel <karldrummer12@gmail.com>
2022-08-10 19:21:59 -0700

    Update SantaCruz_LinkedList_1.1.cpp

--- SantaCruz_LinkedList_1.1/SantaCruz_LinkedList_1.1.cpp
+++ SantaCruz_LinkedList_1.1/SantaCruz_LinkedList_1.1.cpp
@@ -1,4 +1,4 @@
-// SantaCruz_LinkedList_1.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
